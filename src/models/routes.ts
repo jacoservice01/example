4e62b320a6443fb13b16b4bf18885573318c43c6
skedar <ruben6b19@gmail.com>
2023-07-25 09:43:36 -0400

    first commit

---
+++ src/models/routes.ts
@@ -0,0 +1,11 @@
+export const PublicRoutes = {
+  LOGIN: 'login'
+};
+
+export const PrivateRoutes = {
+  PRIVATE: 'private',
+  DASHBOARD: 'Examples',
+  HOME: 'Home',
+  CONTACT: 'Contact',
+  SEARCH: 'Search'
+}
